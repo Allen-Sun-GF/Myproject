f696657dde59db9915f5009da9aa2874e9766df2
Allen <allen840709@gmail.com>
2020-11-29 21:30:51 +0800

    Initial commit

---
+++ src/main/java/com/allen/Hello.java
@@ -0,0 +1,10 @@
+package com.allen;
+
+public class Hello {
+    public static void main(String[] args) {
+        //System.out.println("Hello Java");
+        //new Person().hello();
+        Person p = new Person(); //宣告可以放Person的物件p
+        p.hello();
+    }
+}
