7c2801fe1c59e8cde4457073cecb59fcb44f4796
Allen <allen840709@gmail.com>
2020-11-30 23:17:08 +0800

    Java and Kotlin variable, data type

--- src/main/java/com/allen/Hello.java
+++ src/main/java/com/allen/Hello.java
@@ -4,7 +4,21 @@ public class Hello {
     public static void main(String[] args) {
         //System.out.println("Hello Java");
         //new Person().hello();
-        Person p = new Person(); //宣告可以放Person的物件p
+        Person p = new Person();
         p.hello();
+        int age = 19;
+        Integer age4 = 19;
+        short age2 = 19;
+        long age3 = 19;
+        char c = 'A';
+        Character c2 = 'B';
+
+        byte b =120;
+        float weight = 66.5f;
+        double height = 1.7;
+        boolean adult = true;
+        boolean enroll = false;
+        String name = "Allen";
+
     }
 }
--- src/main/kotlin/com/allen/Hello.kt
+++ src/main/kotlin/com/allen/Hello.kt
@@ -5,6 +5,11 @@ fun main(args: Array<String>) {
     //Human().hello()
     val h = Human()
     h.hello()
+    var age = 19
+    age = 20
+    val name = "Allen"
+    var name2 : String
+    name2 = "Allen2"
 }
 
 class Human {
